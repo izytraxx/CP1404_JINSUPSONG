8717200dbbc0add11072fb98893bad920c9ab5b1
JINSUP SONG <jinsup.song@my.jcu.edu.au>
2017-08-17 16:57:12 +0800

    Practical 05

--- Prac05/color-names.py
+++ Prac05/color-names.py
@@ -1,7 +1,7 @@
 from operator import itemgetter
 
 COLORS = {"AliceBlue": "#f0f8ff", "beige": "#f5f5dc", "black": "#000000", "brown": "#a52a2a", "coral": "#ff7f50", "DimGray": "#696969", "firebrick": "#b22222", "gray": "#bebebe", "lavender": "#e6e6fa", "khaki": "#f0e68c"}
-COLORS = {k.lower(): v for k, v in COLORS.items()}
+COLORS = {k.lower(): v for k, v in COLORS.items()} #turns every key in dict into lowercase
 
 color_input = input("Enter the color: ").lower()
 while not color_input in COLORS:
@@ -9,7 +9,6 @@ while not color_input in COLORS:
     color_input = input("Enter the color: ").lower()
 
 print(COLORS[color_input])
-
 
 
 
